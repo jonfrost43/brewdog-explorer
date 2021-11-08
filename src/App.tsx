f651a3fc116d27cd8e69cb7921c2384d62baa132
Jon Frost <jon.frost@yell.com>
2021-11-08 15:50:12 +0000

    prettier endOfLine auto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import Home from './views/Home';
 import Search from './views/Search';
 import Beer from './views/Beer';
@@ -7,26 +9,28 @@ import styles from './App.module.css';
 
 function App() {
     return (
-        <div className={styles.app}>
-            <BrowserRouter>
-                <header className={styles.header}>
-                    <h1>
-                        <Link to="/">brewdog-explorer</Link>
-                    </h1>
-                </header>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/search/:year">
-                        <Search />
-                    </Route>
-                    <Route path="/beer/:id">
-                        <Beer />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
+        <Provider store={store}>
+            <div className={styles.app}>
+                <BrowserRouter>
+                    <header className={styles.header}>
+                        <h1>
+                            <Link to="/">brewdog-explorer</Link>
+                        </h1>
+                    </header>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/search/:year">
+                            <Search />
+                        </Route>
+                        <Route path="/beer/:id">
+                            <Beer />
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        </Provider>
     );
 }
 
